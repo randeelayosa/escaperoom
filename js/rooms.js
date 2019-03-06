a95f198ccd068ea2b2547d8d1d50a21b2ae600d3
Matt Groberg and Randee Layosa <randee@>
2019-03-06 14:18:46 -0800

    Implement different code for masking and cursor tracking.

--- js/rooms.js
+++ js/rooms.js
@@ -1,35 +1,13 @@
-jQuery(document).ready(function($) {
+$(document).ready(function(){
+  let container = document.querySelector('.frame');
+  const RADIUS = 300;
 
-	// set the variables
-	var timer;
-    var mouseX = 0, mouseY = 0;
-    var xp = 0, yp =0;
-    var circle = $("#circle");
+  document.addEventListener('mousemove', function (event) {
+   let x = event.clientX;
+   let y = event.clientY;
 
-    function mouseStopped(){
-        // if mouse stop moving remove class moving
-        // it will hide the circle with opacity transition
-        circle.removeClass('moving');
-    }
-
-    $(document).mousemove(function(e){
-    	// if mouse start moving add class moving
-        // it will show the circle with opacity transition
-    	circle.addClass('moving');
-    	// get the mouse position minus 160px to center the circle
-        mouseX = e.pageX - 160;
-        mouseY = e.pageY - 160;
-        // if mouse stop moving clear timer and call mouseStopped function
-        clearTimeout(timer);
-        timer=setTimeout(mouseStopped,3000);
-    });
-
-    // set the momentum with setInterval function
-    var loop = setInterval(function(){
-       // change 12 to alter damping higher is slower
-       xp += ((mouseX - xp)/6);
-       yp += ((mouseY - yp)/6);
-       circle.css({left: xp +'px', top: yp +'px'});  //
-    }, 30);
-
+   let circle = `circle(${RADIUS}px at ${x}px ${y}px)`;
+   container.style['-webkit-clip-path'] = circle;
+   container.style['clip-path'] = circle;
+  });
 });
