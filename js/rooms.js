707725c20ee685e95781bdf241ba7bafeb3cc4f6
Matt Groberg and Randee Layosa <randee@>
2019-03-07 14:21:38 -0800

    add house navigation

--- js/rooms.js
+++ js/rooms.js
@@ -1,16 +1,29 @@
 $(document).ready(function(){
-  setTimeout(function(){
-    
-  },10000)
-  let container = document.querySelector('.lights-out');
-  const RADIUS = 200;
 
-  document.addEventListener('mousemove', function (event) {
-   let x = event.clientX;
-   let y = event.clientY;
+    let container = document.querySelector('.lights-on');
+    const RADIUS = 200;
 
-   let circle = `circle(${RADIUS}px at ${x}px ${y}px)`;
-   container.style['-webkit-clip-path'] = circle;
-   container.style['clip-path'] = circle;
-  });
+    document.addEventListener('mousemove', function (event) {
+      let x = event.clientX;
+      let y = event.clientY;
+
+      let circle = `circle(${RADIUS}px at ${x}px ${y}px)`;
+      container.style['-webkit-clip-path'] = circle;
+      container.style['clip-path'] = circle;
+    });
+
+    // function mouseStopped(){
+    //     // if mouse stop moving remove class moving
+    //     // it will hide the circle with opacity transition
+    //     container.removeClass('moving');
+    // }
+    //
+    // $(document).mousemove(function(e){
+    // 	// if mouse start moving add class moving
+    //     // it will show the circle with opacity transition
+    // 	container.addClass('moving');
+    //     // if mouse stop moving clear timer and call mouseStopped function
+    //     clearTimeout(timer);
+    //     timer=setTimeout(mouseStopped,3000);
+    // });
 });
